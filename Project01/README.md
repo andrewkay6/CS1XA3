20d5f4349212fb66e3cea8ea27d2e879cc8a632d
andrewkay6 <kaya6@mcmaster.ca>
2019-02-23 22:27:17 -0500

    added 'file type count' option and updated readme

--- Project01/README.md
+++ Project01/README.md
@@ -1,3 +1,5 @@
 project01_script.sh is the main script file described in the project. It currently accepts both
-user input and system input. 1 echoes back whatever the user inputs, and 2 creates a TODO log with
-the contents of every file in the repo + '#TODO' at the end of each line.
+user input and system input.
+1 echoes back whatever the user inputs
+2 creates a TODO log that contains all of the lines in the project directory that have the #TODO tag
+3 lists the amount of HTML, Javascript, CSS, Python, Haskell, and Bash Script files there are in the project folder
--- Project01/project01_script.sh
+++ Project01/project01_script.sh
@@ -6,8 +6,16 @@ commands () {
       echo $userWord
   elif [ "$1" = 2 ]
   then
-    #find ~/CS1XA3 -type f -name '*' -exec grep -L "#todo" {} + > TODO.log
     grep -r -h -s '#TODO' ~/CS1XA3 > TODO.log
+  elif [ "$1" = 3 ]
+  then
+    echo "HTML: `find ~/CS1XA3 -name "*.html" -type f | wc -l` "
+    echo "Javascript: `find ~/CS1XA3 -name "*.js" -type f | wc -l` "
+    echo "CSS: `find ~/CS1XA3 -name "*.css" -type f | wc -l` "
+    echo "Python: `find ~/CS1XA3 -name "*.py" -type f | wc -l` "
+    echo "Haskell: `find ~/CS1XA3 -name "*.hs" -type f | wc -l` "
+    echo "Bash: `find ~/CS1XA3 -name "*.sh" -type f | wc -l` "
+
 
   fi
 }
@@ -17,6 +25,7 @@ then
     echo "Which command would you like to execute?"
     echo "1) output a word"
     echo "2) create TODO log"
+    echo "3) file type count"
     read varInput
     commands $varInput
 else
